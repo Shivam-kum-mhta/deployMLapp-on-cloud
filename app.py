77e679c6522415613faadc09ccf3a14c61857975
Shivam-Kumar-A <155562554+Shivam-kum-mhta@users.noreply.github.com>
2024-08-08 00:00:48 +0530

    Update app.py
    Signed-off-by: Shivam-Kumar-A <155562554+Shivam-kum-mhta@users.noreply.github.com>

--- app.py
+++ app.py
@@ -4,49 +4,13 @@ from transformers import DistilBertTokenizer, DistilBertForSequenceClassificatio
 import torch
 import logging
 import os
+
 app = FastAPI()
 
-from fastapi.middleware.cors import CORSMiddleware
 
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
-
-class TextInput(BaseModel):
-    text: str
-
-# Load tokenizer and model
-model_path = "shivamkumaramehta/Search-Shield"  # Replace with your model path
-token = "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"  # Replace with your Hugging Face token
-
-tokenizer = DistilBertTokenizer.from_pretrained(model_path)
-model = DistilBertForSequenceClassification.from_pretrained(model_path, revision="main", token=token)
-
-# Function to predict profanity
-def predict_profanity(text):
-    inputs = tokenizer(text, truncation=True, padding=True, max_length=512, return_tensors="pt")
-    with torch.no_grad():
-        outputs = model(**inputs)
-    logits = outputs.logits
-    predicted_class = logits.argmax().item()
-    return predicted_class
-
-# Endpoint to predict profanity
-@app.post("/predict-profanity/")
-async def predict_profanity_endpoint(input: TextInput, request: Request):
-    try:
-        logging.info(f"Received request: {await request.json()}")
-        predicted_class = predict_profanity(input.text)
-        logging.info(f"Predicted class: {predicted_class}")
-        return {"predicted_class": predicted_class}
-    except Exception as e:
-        logging.error(f"Error processing request: {str(e)}")
-        raise HTTPException(status_code=500, detail=str(e))
-
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
 if __name__ == "__main__":
     import uvicorn
     logging.basicConfig(level=logging.INFO)
